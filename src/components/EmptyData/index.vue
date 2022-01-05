257b48226c0cecac6d3494f928e83ac157b49681
Fabian Skarmeta <fabian.skarmeta@gmail.com>
2022-01-05 18:17:03 -0300

    ADD: Empty Data component

---
+++ src/components/EmptyData/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <a-result class="no-results-box" status="warning" title="No results found">
+    <template #icon>
+      <a-icon type="close-circle" theme="filled" />
+    </template>
+    <template #extra>
+      <a-button>Back</a-button>
+    </template>
+  </a-result>
+</template>
+
+<script>
+export default {
+  name: "EmptyDataComponent",
+};
+</script>
+
+<style lang="scss" scoped>
+.no-results-box {
+  margin: 64px 0;
+}
+</style>
