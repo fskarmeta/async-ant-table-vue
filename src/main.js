4ecff47866f86f444a22ccb8357578aa1b569949
Fabian Skarmeta <fabian.skarmeta@gmail.com>
2022-01-05 18:16:39 -0300

    ADD: Import first components

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
+import { Button, Collapse, Result, Icon } from "ant-design-vue";
 
 Vue.config.productionTip = false;
+Vue.use(Button);
+Vue.use(Collapse);
+Vue.use(Result);
+Vue.use(Icon);
 
 new Vue({
   render: (h) => h(App),
