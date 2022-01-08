e58f8d54844868626cce2af06824dfc64c593029
Fabian Skarmeta <fabian.skarmeta@gmail.com>
2022-01-07 21:07:50 -0300

    ADD: Dummy fetched data component

---
+++ src/components/FetchedData/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="fetched-data-container">
+    <p>Your fetched data with</p>
+    <p>{{ data.number }}</p>
+    <p>{{ data.name }}</p>
+    <p>{{ data.state }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FetchedDataComponent",
+  props: {
+    data: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.fetched-data-container {
+  margin-top: 16px;
+  width: 100%;
+  text-align: center;
+}
+</style>
