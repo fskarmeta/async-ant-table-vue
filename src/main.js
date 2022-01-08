c31b207c12ea0b31884fd4ca46fe3dca976ec9f6
Fabian Skarmeta <fabian.skarmeta@gmail.com>
2022-01-07 21:06:19 -0300

    CHORE: Components importations

--- src/main.js
+++ src/main.js
@@ -1,12 +1,28 @@
 import Vue from "vue";
 import App from "./App.vue";
-import { Button, Collapse, Result, Icon } from "ant-design-vue";
+import {
+  Button,
+  Collapse,
+  Result,
+  Icon,
+  Table,
+  notification,
+  Badge,
+  Spin,
+  Card,
+} from "ant-design-vue";
 
 Vue.config.productionTip = false;
 Vue.use(Button);
 Vue.use(Collapse);
 Vue.use(Result);
 Vue.use(Icon);
+Vue.use(Table);
+Vue.use(Badge);
+Vue.use(Spin);
+Vue.use(Card);
+
+Vue.prototype.$notification = notification;
 
 new Vue({
   render: (h) => h(App),
