2b05d8133a2d0bf27f70fcde625b6223b14bac15
Fabian Skarmeta <fabian.skarmeta@gmail.com>
2022-01-07 21:06:40 -0300

    ADD: Main Component and name changes

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <CollapseTable />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import CollapseTable from "./components/CollapseTable.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    CollapseTable,
   },
 };
 </script>
